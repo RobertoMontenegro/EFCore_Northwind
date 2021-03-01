7e5633848e0ec56ae8159ba99087a9222fc0b64c
RobertoMontenegro <74753417+RobertoMontenegro@users.noreply.github.com>
2021-02-28 22:01:23 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Tarea1_EFCore.Models;
+
+namespace Tarea1_EFCore
+{
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            northwindContext DataContext = new northwindContext();
+
+            var empleados = DataContext.Employees
+                .Where(w => w.Country == "UK")
+                .Select(s => new 
+            { 
+                s.FirstName,
+                s.LastName,
+                s.City
+            });
+            var salida = empleados.ToList();
+
+        }
+    }
+}
